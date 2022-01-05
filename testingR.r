d139fd6030c3bb63ab4e14b1f95904b85b4747f3
Osmanli125 <osmanlisalar@gmail.com>
2022-01-05 09:54:46 +0500

    added few lines

--- testingR.r
+++ testingR.r
@@ -1,2 +1,6 @@
-Creating repo for R-programming with a file name testingR.r
+R-programming 
 print("As-Salamu-Alykum")
+name = "RSTUDIO"
+excercise = "version_control_and_linking"
+print(name)
+print(excercise)
