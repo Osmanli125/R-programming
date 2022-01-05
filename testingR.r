4e4e6d28e168dae144cf9bae0682f51fd370351b
Osmanli125 <osmanlisalar@gmail.com>
2022-01-05 10:00:10 +0500

    Added few lines

--- testingR.r
+++ testingR.r
@@ -1,6 +1,9 @@
-R-programming 
+lang = "Rprogramming" 
 print("As-Salamu-Alykum")
 name = "RSTUDIO"
 excercise = "version_control_and_linking"
 print(name)
 print(excercise)
+Statement = "it is important to save the file first and then try to commit"
+print(Statement)
+print(lang)
